0b16ce30f54824871841b813d1d297476038d78d
lisa21212 <adrian199988@gmail.com>
2021-02-23 21:32:31 +0800

    Adrian Lee

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi! I'm Adrian.</Text>
+      <Text>Hi! I'm Adrian Lee.</Text>
       <StatusBar style="auto" />
     </View>
   );
