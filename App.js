ad59386e2c6da9581db8d91d2e0deb8ce7bb026a
lisa21212 <adrian199988@gmail.com>
2021-02-24 16:21:55 +0800

    add Click.js

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState,useEffect}  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Click from './Click';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Hi! I'm Adrian Lee.</Text>
-      <StatusBar style="auto" />
+      <View style={styles.top}/>
+      <View style={styles.body}/>
+      {/*<Text>Hi! I'm Adrian Lee.</Text>
+      <Click count={10}/>
+  <StatusBar style="auto" />*/}
+  <View style={styles.bottom} />
     </View>
+
+  
+
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+    /*backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  */
   },
+  bottom: {
+    height: 80,
+    backgroundColor: '#808B96',
+  },
+  top:{
+    height: 80,
+    backgroundColor: '#E74C3C',
+  },
+  body:{
+    
+    backgroundColor:'#e5ffff',
+  }
 });
